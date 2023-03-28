f8fa5d6e087938abec650f3097507ea834d7e220
Chiang <tien23@gmail.com>
2023-03-28 14:11:07 +0800

    feat: modify home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,21 @@
 const router = require('express').Router()
+const Record = require('../../models/recordSchema')
+const Category = require('../../models/categorySchema')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  try {
+    const userId = req.user._id
+    const records = await Record.find({ userId }).lean()
+
+    let totalAmount = 0
+    for (const record of records) {
+      totalAmount += Number(record.amount)
+    }
+
+    res.render('index', { records, totalAmount })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
