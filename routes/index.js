2bb39e39d7b5a30df241184da9ec304236b93371
Chiang <tien23@gmail.com>
2023-03-29 14:27:25 +0800

    feat: add filter feature

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const home = require('./modules/home')
 const users = require('./modules/users')
 const { authecticator } = require('../middleware/auth')
 const records = require('./modules/records')
+const filter = require('./modules/filter')
 
 router.use('/users', users)
 router.use('/records', authecticator, records)
+router.use('/filter', authecticator, filter)
 router.use('/', authecticator, home)
 
 module.exports = router
---
+++ routes/modules/filter.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
+router.get('/', async (req, res) => {
+  try {
+    const userId = req.user._id
+    const { category } = req.query
+
+    if (!category) return res.redirect('/')
+    const foundRecords = await Record.find({ userId, category }).lean()
+    let totalAmount = 0
+
+    const records = []
+    for (const record of foundRecords) {
+      totalAmount += record.amount
+      const foundCategory = await Category.findById({ _id: record.categoryId })
+      record.icon = foundCategory.icon
+      record.date = record.date.toISOString().slice(0, 10)
+      records.push(record)
+    }
+
+    res.render('index', { records, totalAmount, category })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+module.exports = router
