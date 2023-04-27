8ada0b1e00566af4911430dd4d087a062af5b999
tschiang23 <tien23@gmail.com>
2023-04-27 19:05:49 +0800

    feat: add register validator

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
-  res.locals.error = req.flash('error')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const User = require('../../models/user')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
+const { body, validationResult } = require('express-validator');
 
 // login
 router.get('/login', (req, res) => {
@@ -22,31 +23,52 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', [
+  body('name')
+    .trim()
+    .notEmpty().withMessage('姓名不能為空白')
+    .bail() //第一個條件不通過 不繼續檢查
+    .isLength({ min: 2, max: 20 }).withMessage('姓名至少需兩個字以上，最多二十個字')
+  ,
+  body('email')
+    .isEmail().withMessage('必須是合法email')
+    .bail()
+    .isLength({ max: 32 })
+    .custom(async (email) => {
+      const foundUser = await User.findOne({ email })
+      if (foundUser) {
+        throw new Error('Email已經被註冊')
+      }
+    })
+  ,
+  body('password').trim().isLength({ min: 6, max: 32 }).withMessage('密碼長度至少六位'),
+  body('confirmPassword')
+    .trim()
+    .custom((value, { req }) => {
+      // 確認密碼欄位的值需要和密碼欄位的值相符
+      if (value !== req.body.password) {
+        // 驗證失敗時的錯誤訊息
+        throw new Error('兩次輸入的密碼不相同')
+      }
+      // 成功驗證回傳 true
+      return true
+    })
+], async (req, res) => {
   try {
     let { name, email, password, confirmPassword } = req.body
-    const hash = await bcrypt.hash(password, 10)
 
-    const foundUser = await User.findOne({ email })
-
-    const errors = []
-    if (foundUser) {
-      errors.push({ message: '帳號已經被註冊' })
-    }
-
-    if (password !== confirmPassword) {
-      errors.push({ message: '密碼與確認密碼不相符' })
-    }
-
-    if (errors.length) {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
       return res.render('register', {
-        errors,
+        errors: result.array(),
         name,
         email,
         password,
         confirmPassword,
       })
     }
+
+    const hash = await bcrypt.hash(password, 10)
 
     await User.create({
       name,
